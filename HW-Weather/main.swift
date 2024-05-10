02e4f67f529f1c858c2167bee4dc9cdd984a4c90
Pavel <rezusx@mail.ru>
2024-05-10 14:22:16 +0300

    init

--- HW-Weather/main.swift
+++ HW-Weather/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+let urlString = "https://api.openweathermap.org/data/2.5/weather?q=Moscow&APPID=4f7dd0216b1e869d2a717e512544f020"
 
+guard let url = URL(string: urlString) else {
+    print("Incorrect URL")
+    exit(1)
+}
+
+let semaphore = DispatchSemaphore(value: 0)
+
+URLSession.shared.dataTask(with: url) { data, _, error in
+    defer { semaphore.signal() }
+    guard let data = data else {
+        print(error?.localizedDescription ?? "No Error description")
+        return
+    }
+    let decoder = JSONDecoder()
+    do {
+        let weatherData = try decoder.decode(WeatherData.self, from: data)
+        print(weatherData)
+    } catch {
+        print(error.localizedDescription)
+    }
+}.resume()
+
+semaphore.wait()
